8c62d2c9f1a42f992acb71596bce4d5f3af5ad49
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-25 23:45:06 +0530

    Added implemented program of regular expression for first name validation.

---
+++ src/com/bridgelabz/regex/RegeXMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regex;
+
+public class RegeXMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+        RegeXOperation operation = new RegeXOperation();
+        operation.userFirstName();
+    }
+}
---
+++ src/com/bridgelabz/regex/RegeXOperation.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegeXOperation {
+    static void userFirstName() {
+
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+
+        if(result == true)
+            System.out.println("First Name is Valid");
+        else
+            System.out.println("First Name is Invalid");
+    }
+}
